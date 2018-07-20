401056cffc532fe73599dae25c28ccc40dd0f4c5
astrebeck <alexstrebeck@gmail.com>
2018-07-20 10:08:22 -0600

    ok tests for binary_search_tree

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,10 +5,65 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    node_start = self
+    cb(node_start.value)
+    node = node_start.left
+    rights = []
+    lefts = []
+    
+    while(node):
+
+      cb(node.value)
+
+      if(node.right):
+
+        rights.append(node.right)
+
+      node = node.left
+
+    for obj in rights:
+
+      cb(obj.value)
+
+    node = node_start
+    node = node.right
+
+    while(node):
+
+      cb(node.value)
+
+      if(node.left):
+        lefts.append
+
+      node = node.right
+
+    for obj in lefts:
+
+      cb(node.value)
+
+
+
 
   def breadth_first_for_each(self, cb):
-    pass
+    node = self
+    nodes = [node]
+
+    for obj in nodes:
+
+      if not obj.left in nodes and obj.left != None:
+        nodes.append(obj.left)
+
+      if not obj.right in nodes and obj.right != None:
+        nodes.append(obj.right)
+
+    for obj in nodes:
+
+      cb(obj.value)
+
+    print(nodes)
+
+
+
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
